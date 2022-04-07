153bdae44e3ce5a0d09f2f2664c19ef15099d138
Silverio Moraes <silverstone191013@gmail.com>
2022-04-07 18:09:46 -0300

    7°programinha-correção

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,22 +1,22 @@
 const nota = 65;
 
-if (nota >= 90){
+if (nota >= 90 && nota < 100){
     console.log("A");
 }    
-else if (nota >= 80){
+else if (nota >= 80 && nota < 100){
     console.log("B");
 }
-else if (nota >= 70){
+else if (nota >= 70 && nota < 100){
     console.log("C");
 }
-else if (nota >= 60){
+else if (nota >= 60 && nota < 100){
     console.log("D");
 }
-else if (nota >= 50){
+else if (nota >= 50 && nota < 100){
     console.log("E");
 }
 else if (nota < 50 && nota > 0){
     console.log("F");
 }
-else (nota < 0 || nota >100)
+else if (nota < 0 && nota >100)
     console.log("error")
