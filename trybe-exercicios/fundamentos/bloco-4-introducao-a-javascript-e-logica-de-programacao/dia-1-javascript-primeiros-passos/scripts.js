536d1f352d216d4ed07cb66fb1a9b6ff077675be
Silverio Moraes <silverstone191013@gmail.com>
2022-04-07 10:12:21 -0300

    3°programinha

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,9 +1,13 @@
-const a =100;
-const b =20;
+const a = 100;
+const b = 20;
+const c = 180;
 
-if (100>20){
+if (100>20 && 100>180){
 console.log(a);
 }
-else {
+else if (20>100 && 20>180){
     console.log(b);
+}
+else {
+    console.log(c);
 }
