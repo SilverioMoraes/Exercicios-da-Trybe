13bcc70754d5011990482b06fc089103e9b9cdfa
Silverio Moraes <silverstone191013@gmail.com>
2022-04-07 15:34:53 -0300

    5°programinha

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,12 +1,13 @@
-const a = 33;
-
-if (a>0) {
-    console.log("positive");
-}
-else if (a<0) {
-    console.log("negative");
-}
+const a = 45;
+const b = 45;
+const c = 90;
+if (a > 0 && b > 0 && c > 0)
+if (a+b+c == 180 ) {
+    console.log(true);
+}  
 else {
-    console.log("zero");
-
+    console.log(false);
+}   
+else {
+    console.log("ERROR");
 }
