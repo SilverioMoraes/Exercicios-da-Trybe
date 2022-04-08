d48bc52ded395f793f922435ab2c173c9f2fcd7e
Silverio Moraes <silverstone191013@gmail.com>
2022-04-08 00:14:33 -0300

    exercicio-7

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,14 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
+let menorNumero = numbers[0];
 
-for (let i = 0; i < numbers.length; i++){
-    if (numbers[i] % 2 !== 0) {
-        resultado ++;
+for (let i = 1; i < numbers.length; i++){
+    if (numbers[i] < menorNumero) {
+        menorNumero = numbers[i];
     }
 }
-if (resultado == 0) {
-    console.log("nenhum valoe impar encontrado");
-}
-else {
-    console.log(resultado);
-}
+console.log(menorNumero)
