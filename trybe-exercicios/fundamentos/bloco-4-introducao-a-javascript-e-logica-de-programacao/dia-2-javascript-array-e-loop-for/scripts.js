e2010542be60f3d675ed6f8dea82603fd1e48451
Silverio Moraes <silverstone191013@gmail.com>
2022-04-08 00:55:20 -0300

    exercicio-bonus-2

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -2,7 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let i = 1; i < numbers.length; i ++) {
     for (let index = 0; index < i; index ++) {
-      if (numbers[i] < numbers[index]) {
+      if (numbers[i] > numbers[index]) {
         let ordem = numbers[i];
         numbers[i] = numbers[index];
         numbers[index] = ordem;
