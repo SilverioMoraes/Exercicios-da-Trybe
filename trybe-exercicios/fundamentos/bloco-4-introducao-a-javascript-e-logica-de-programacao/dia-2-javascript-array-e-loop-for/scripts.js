d941e9a6a39a0dd17698aee320175ed8bd2ac88d
Silverio Moraes <silverstone191013@gmail.com>
2022-04-07 23:49:51 -0300

    exercicio-5

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,14 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
+let maiorNumero = numbers[0];
 
-for(let i = 0; i < numbers.length; i++) {
-resultado += numbers[i];
+for (let i = 0; i < numbers.length; i++){
+    if (numbers[i] > maiorNumero) {
+        maiorNumero = numbers[i];
+    }
 }
-let média = resultado / numbers.length;
-
-if (média > 20) {
-    console.log("valor maior que 20");
-}
-else {
-    console.log("valor menor ou igual a 20");
-}
+console.log(maiorNumero);
