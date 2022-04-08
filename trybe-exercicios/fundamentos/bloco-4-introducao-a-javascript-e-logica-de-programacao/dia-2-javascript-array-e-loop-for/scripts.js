ca639bd421bbd1b3447f905b7848195491e8e204
Silverio Moraes <silverstone191013@gmail.com>
2022-04-08 00:28:23 -0300

    exercicio-9

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -1,6 +1,5 @@
-let numbers = [];
+let numbers = [1,  2,  3,  4,  5,  6,  7,  8 ,9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
-for (let i = 1; i <= 25; i++){
-   numbers.push(i);
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i] / 2);
 }
-console.log(numbers);
