ab35f2bd58d32e70cc8603c69c3f08bdd08345c2
Silverio Moraes <silverstone191013@gmail.com>
2022-04-11 18:57:14 -0300

    exercicio-3

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -3,5 +3,7 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 }
-    info ['recorrente'] = 'sim'
-  ;console.log(info);
+    info ['recorrente'] = 'sim';
+    for (let index in info) {
+        console.log(index)
+    }
