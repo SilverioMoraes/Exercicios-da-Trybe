db7689377ec680087d1ce44d930c74631c9edc4f
Silverio Moraes <silverstone191013@gmail.com>
2022-04-11 20:06:18 -0300

    exercicio-5

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -2,8 +2,19 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+};
+let info2 ={
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',   
+};
+for(let index in info) {
+    if(info[index] == 'sim' && info2[index] == 'sim' && index =='recorrente'){
+    console.log('iguais');
 }
-    info ['recorrente'] = 'sim';
-    for (let index in info) {
-        console.log(info[index])
-    }
+else{
+    console.log(info[index]+' e '+info2[index]);
+}
+}
