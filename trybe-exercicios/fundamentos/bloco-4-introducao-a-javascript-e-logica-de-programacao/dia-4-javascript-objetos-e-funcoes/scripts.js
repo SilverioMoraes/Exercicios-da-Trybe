c23557134d5ad7b66d327b281f5c2bbe9378a2e2
Silverio Moraes <silverstone191013@gmail.com>
2022-04-11 21:17:24 -0300

    exercicio-3-parte||

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -1,12 +1,12 @@
-let numbers =[2, 3, 6, 7, 10, 1];
-function indiceDoMaior(numbers) {
-    let indiceMaior = 0;
+let numbers =[2, 4, 6, 7, 10, 0, -3];
+function indiceDoMenor(numbers) {
+    let indiceMenor = 0;
     for (let index in numbers) {
-      if (numbers[indiceMaior] < numbers[index]) {
-        indiceMaior = index;
+      if (numbers[indiceMenor] > numbers[index]) {
+        indiceMenor = index;
       }
     }
-    return indiceMaior;
+    return indiceMenor;
   }
   
-  console.log(indiceDoMaior(numbers));
+  console.log(indiceDoMenor(numbers));
