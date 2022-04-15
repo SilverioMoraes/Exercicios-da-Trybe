8ecb49512caca6617c7797cf5b6d37ab26637119
Silverio Moraes <silverstone191013@gmail.com>
2022-04-14 22:56:28 -0300

    exercicio-3

--- trybe-exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/scripts.js
+++ trybe-exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/scripts.js
@@ -1,2 +1,3 @@
 document.getElementsByTagName("p")[1].innerText ="Eu me vejo trabalhando como desenvolvedor e adiquindo ainda mais conhecimento."
 document.getElementsByTagName("main")[0].style.backgroundColor = "rgb(76,164,109)";
+document.getElementsByClassName("center-content")[0].style.backgroundColor = "white";
