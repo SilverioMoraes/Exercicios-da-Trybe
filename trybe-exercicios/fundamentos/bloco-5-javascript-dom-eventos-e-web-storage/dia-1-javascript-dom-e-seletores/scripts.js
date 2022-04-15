84a5b8df4b367096e3bddda21d14e06758ee1e7f
Silverio Moraes <silverstone191013@gmail.com>
2022-04-14 22:51:05 -0300

    exercicio-1

--- trybe-exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/scripts.js
+++ trybe-exercicios/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/scripts.js
@@ -1,11 +1,2 @@
-document.getElementById("header-container").style.backgroundColor = "green";
-document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "salmon";
-let emergency = document.getElementsByTagName("h3");
-for(let i = 0; i < emergency.length-2;i ++){
-emergency[i].style.backgroundColor = "indigo"
-}
-document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "yellow";
-let noEmergency = document.getElementsByTagName("h3");
-for(let i = 2; i < noEmergency.length;i ++){
-emergency[i].style.backgroundColor = "black"
-}
+document.getElementsByTagName("p")[1].innerText ="Eu me vejo trabalhando como desenvolvedor e adiquindo ainda mais conhecimento."
+document.getElementsByTagName("main")[0].style.backgroundColor = "rgb(76,164,109)";
