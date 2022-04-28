d94c139910f373f95a9ae8224d42354f7743a2d7
Silverio Moraes <silverstone191013@gmail.com>
2022-04-28 19:54:29 -0300

    exercio-2

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -1,6 +1,13 @@
 let fatorial = 1;
-
-for (let index = 10; index > 0; index --) {
+for (let index = 10; index > 0; index --)
+ {
   fatorial *= index;
 }
 console.log(fatorial);
+
+let word = 'tryber';
+let inversa = '';
+for (let i = 0; i < word.length; i ++) {
+ inversa += word[word.length - 1 - i];
+}
+console.log(inversa);
