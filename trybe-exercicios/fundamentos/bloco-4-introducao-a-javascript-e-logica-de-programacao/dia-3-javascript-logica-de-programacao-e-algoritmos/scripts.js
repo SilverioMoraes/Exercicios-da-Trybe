28651185f0a56d3411631e690c1490f0564188b1
Silverio Moraes <silverstone191013@gmail.com>
2022-04-28 20:24:36 -0300

    exercio-3

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -1,6 +1,5 @@
 let fatorial = 1;
-for (let index = 10; index > 0; index --)
- {
+for (let index = 10; index > 0; index --) {
   fatorial *= index;
 }
 console.log(fatorial);
@@ -11,3 +10,20 @@ for (let i = 0; i < word.length; i ++) {
  inversa += word[word.length - 1 - i];
 }
 console.log(inversa);
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array [0];
+let menorPalavra = array [0];
+for (let i = 0; i < array.length; i ++) {
+  if (array[i].length > maiorPalavra.length) {
+    maiorPalavra = array[i];
+  }
+}
+console.log(maiorPalavra);
+for (let i = 0; i < array.length; i ++) {
+  if (array[i].length < menorPalavra.length) {
+    menorPalavra = array[i];
+  }
+}
+console.log(menorPalavra);
+
