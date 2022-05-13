41fac203fab1c9f674051bb851d0d8e1e8307ed0
Silverio Moraes <silverstone191013@gmail.com>
2022-05-13 19:37:49 -0300

    exercicio-1

---
+++ trybe-exercicios/fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio-1.js
@@ -0,0 +1,15 @@
+const employee = (nomeCompleto) => {
+    const email = nomeCompleto.toLowerCase().replace(' ', '_');
+    return {nomeCompleto, email:`${email}@trybe.com`};
+}
+console.log(employee('Pedro Guerra'));
+
+const newEmployees = (callback) => {
+    const employees = {
+      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: callback('Luiz Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+  };
+//   console.log(newEmployees(employee));
