b8ce2d8668e2a410a56f41121abd40d5ca5818c8
Silverio Moraes <silverstone191013@gmail.com>
2022-05-16 20:47:02 -0300

    exercicio-2

--- trybe-exercicios/fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
+++ trybe-exercicios/fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise1.js
@@ -65,4 +65,16 @@ const books = [
   function authorBornIn1947() {
     return books.find((Pessoa) => Pessoa.author.birthYear === 1947).author.name;
   }
-  console.log(authorBornIn1947());
+//   console.log(authorBornIn1947());
+
+function smallerName() {
+    let nameBook;
+    books.forEach((book) => {
+        if (!nameBook || book.name.length < nameBook.length){;
+          nameBook = book.name;
+        }
+    });
+    // Variável nameBook que receberá o valor do menor nome;
+    return nameBook;
+  }
+//   console.log(smallerName());
