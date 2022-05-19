efa289cc3f90a2300e2338219285cf10ea675b80
Silverio Moraes <silverstone191013@gmail.com>
2022-05-19 18:49:31 -0300

    exercicio-2

--- trybe-exercicios/fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio1.js
+++ trybe-exercicios/fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercicio1.js
@@ -2,6 +2,7 @@ const rectangleArea = (width, height) => width * height;
 
 const rectangle1 = [1, 2];
 const rectangle2 = [3, 5];
+
 const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
